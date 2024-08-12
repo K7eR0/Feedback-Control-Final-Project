09bc20682b59a5a8b91e29975cfad61f28401e8a
K7eR0 <rcn045@gmail.com>
2024-08-11 21:14:00 -0400

    final code

--- Final_MATLab_Code.m
+++ Final_MATLab_Code.m
@@ -18,22 +18,22 @@ DR = round(-log(OS/100)/sqrt(pi^2 + (log(OS/100)^2)), 3);
 wn = 4/(Ts*DR);
 
 %Calculate the desired real part of the dominant pole
-sd = -DR * wn;
+sd = -DR * wn; 
 
 %Calculate the desire imaginary part of the dominant pole
-wd = wn*sqrt(1-DR^2);
+wd = wn*sqrt(1-DR^2); 
 
 %Find the angle between the dominant pole and the first OL pole from the real axis 
-t1 = mod(atand(wd/(sd-p1)),180);
+t1 = mod(atand(wd/(sd-p1)),180); 
 
 %Find the angle between the dominant pole and the second OL pole from the real axis 
-t2 = mod(atand(wd/(sd-p2)),180);
+t2 = mod(atand(wd/(sd-p2)),180); 
 
 %Calculate the position of the added zero from the derivative of the PID controller
-a = wd/(mod(tand((t1+t2)-180),180))-sd;
+a = wd/(mod(tand((t1+t2)-180),180))-sd; 
 
-%Choose the added zero from the intigral of the PID controller
-r = 7.85;
+%Choose the added zero from the integral of the PID controller
+b = 7.85;
 
 %Set 's' to automatically for transfer functions
 s = tf('s');
@@ -44,8 +44,8 @@ G = 14273/((s+9.67)*(s+50));
 %Combine the original transfer function with the derivative
 Ga = G*(s+a);
 
-%Add the intigral into the transfer function
-G1 = Ga*(s+r)/s;
+%Add the integral into the transfer function
+G1 = Ga*(s+b)/s;
 
 %Calculate the gain of the controller
 L1 = sqrt((a+sd)^2 + wd^2);
@@ -64,4 +64,4 @@ rlocus(G2);
 sgrid (.69,0);
 
 %Plot the step response
-figure(3); step(15*feedback(G2,1))
+figure(2); step(15*feedback(G2,1))
